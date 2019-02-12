94fa695749edc138d9b6c23d3181486d5af0d3b3
woyteka <40150050+woyteka@users.noreply.github.com>
2019-02-12 22:22:44 +0100

    06
    git commit -m 06

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 $(document).ready(function(){
+  
+
     $(".portfolio").click(function(){
-        $('.container-box-right').load('page-portfolio.html .container-box-left');
+        $('.txt-strony').addClass('oneout');
+        $('.txt-internetowe').addClass('twoout');
+        $('.txt-materialy').addClass('treeout');
+        $('.txt-graficzne').addClass('fourout');
+        $('.txt-dlainternetu').addClass('fiveout');
+        $('.txt-idruku').addClass('sixout');
+        $('.txt-info').addClass('sevenout');
+       
+
+       
+
+let strona = function(){
+
+        $('.container-box').load('page-portfolio.html .container-box')
+}
+setTimeout(strona, 1500);
     });
-   
+
+
+
+$('.drewnochem').mouseover(function(){
+    $('.drewnochem').toggleClass('oko');
 });
+
+
+
+
+
+  
+});
+
+
 
 
 
