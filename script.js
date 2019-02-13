c0594c17eeb957f47ca26a61bd33a5cddb33840c
woyteka <40150050+woyteka@users.noreply.github.com>
2019-02-13 12:29:36 +0100

    07

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
+jQuery(document).ready(function($){
   
+
+/* KOD PODŁĄCZONY DO BUTTONA PORTFOLIO */
+
+
 
     $(".portfolio").click(function(){
         $('.txt-strony').addClass('oneout');
@@ -15,20 +19,79 @@ $(document).ready(function(){
 
 let strona = function(){
 
-        $('.container-box').load('page-portfolio.html .container-box')
+        window.location="portfolio.html";
 }
 setTimeout(strona, 1500);
     });
 
 
 
+
+
+/* KOD PODŁĄCZONY DO BUTTONA HOME */
+
+    $(".home").click(function(){
+        $('.txt-strony').addClass('oneout');
+        $('.txt-internetowe').addClass('twoout');
+        $('.txt-materialy').addClass('treeout');
+        $('.txt-graficzne').addClass('fourout');
+        $('.txt-dlainternetu').addClass('fiveout');
+        $('.txt-idruku').addClass('sixout');
+        $('.txt-info').addClass('sevenout');
+       
+
+       
+
+let strona = function(){
+
+        window.location="index.html";
+}
+setTimeout(strona, 1500);
+    });
+
+
+
+
+    
+
+
+/* KOD PODŁĄCZONY DO BUTTONA INFO */
+
+    $(".info").click(function(){
+        $('.txt-strony').addClass('oneout');
+        $('.txt-internetowe').addClass('twoout');
+        $('.txt-materialy').addClass('treeout');
+        $('.txt-graficzne').addClass('fourout');
+        $('.txt-dlainternetu').addClass('fiveout');
+        $('.txt-idruku').addClass('sixout');
+        $('.txt-info').addClass('sevenout');
+       
+
+       
+
+let strona = function(){
+
+        window.location="info.html";
+}
+setTimeout(strona, 1500);
+    });
+
+
+
+
+
+
+
 $('.drewnochem').mouseover(function(){
-    $('.drewnochem').toggleClass('oko');
+    $('.imagea').css('display','flex');
+
+    
 });
 
+$('.drewnochem').mouseout(function(){
+    $('.imagea').css('display','none');
 
-
-
+});
 
   
 });
